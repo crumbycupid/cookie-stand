467f29ac4c53c6441145f442cf1a5523333a48b1
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2022-10-11 01:44:08 -0400

    day 1 updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+'use strict'
 
+let seattleList = document.getElementById('seattleList');
+
+console.log(seattleList);
+
+let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m', '11 a.m', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
+
+let seattle = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  dailyTotal: 0,
+  cookiesSoldEachHourArray: [],
+  getRandomCustomers: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min); // The maximum is inclusive and the minimum is inclusive
+  }
+}
+
+console.log(seattle.getRandomCustomers());
